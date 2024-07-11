5091ae0236a36932c4c4ed9a3943c89494a50885
unknown <pptien.lmht@gmail.com>
2024-07-11 23:18:57 +0700

    Add func tich

---
+++ tich.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+        int result = Multiply(a, b);
+        Console.WriteLine("Tích của " + a + " và " + b + " là: " + result);
+    }
+
+    static int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+}
